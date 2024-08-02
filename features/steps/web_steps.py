1f98eb9a833de3f2145dd72ecb3873937c4185c5
kclams <134506470+kclams@users.noreply.github.com>
2024-08-02 15:01:39 +0800

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -106,6 +106,22 @@ def step_impl(context, element_name):
 
 ## UPDATE CODE HERE ##
 
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Firefox()
+driver.get("http://somedomain/url_that_delays_loading")
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+    )
+finally:
+    driver.quit()
+    
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
